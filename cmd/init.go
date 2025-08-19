1f6f3fd101d4413a520a8e4d8be647ca80b0a527
Neil de Carteret <git@lumphammer.net>
2025-08-19 10:02:07 +0100

    improve help

--- cmd/init.go
+++ cmd/init.go
@@ -21,13 +21,8 @@ var baselineConfig = utils.BaselineConfig{
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create a new config file",
+	Long: `Generate template config file at the default location (~/.config/potato.toml)`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// if config file already exists, exit
 		configFilePath = utils.TildeToHomeDir(configFilePath)
